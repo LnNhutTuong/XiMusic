296c1e09bbde041b340a9a62ddf410a2581dbfc3
XimenT <lnnhuttuong@gmail.com>
2026-08-30 15:34:17 +0000

    CI: add fw Jest to test Backend

---
+++ xmt-Backend/src/test/jest-basic.test.js
@@ -0,0 +1,13 @@
+import { add } from "../service/something";
+
+test("add(2,3) equel 5 ?", () => {
+  expect(add(2, 3)).toBe(5);
+});
+
+test("add(0,5) equel 5 ?", () => {
+  expect(add(0, 5)).toBe(5);
+});
+
+test("add(-2,2) equel 0 ?", () => {
+  expect(add(-2, 2)).toBe(0);
+});
